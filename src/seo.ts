69510ca4557b3ae89b3d8ecc2c383388ffe60fa7
Chris Pikul <chris@workmaple.com>
2023-11-01 23:47:20 +0300

    boosted the SEO of articles

--- src/seo.ts
+++ src/seo.ts
@@ -1,6 +1,21 @@
 // @ts-ignore-next-line This is a false error, it compiles just fine.
 import type { Props as SEOProps } from 'astro-seo';
 
+function overrideObjects<T extends Object>(a: T, b: Partial<T>): T {
+    const obj = { ...a };
+
+    for (const key in b) {
+        if (typeof b[key] === 'object') {
+            if (Array.isArray(b[key])) obj[key] = [...b[key]];
+            else obj[key] = overrideObjects(obj[key], b[key]);
+        } else {
+            obj[key] = b[key];
+        }
+    }
+
+    return obj;
+}
+
 /**
  * Creates a new properties object for `astro-seo` by using set defaults and
  * applying custom property overrides if wanted.
@@ -8,42 +23,44 @@ import type { Props as SEOProps } from 'astro-seo';
  * @param overides Optional custom SEO properties
  * @returns New properties object with reasonable defaults applied
  */
-export const MakeSEO = (overrides: SEOProps = {}): SEOProps => ({
-    title: 'Chris Pikul',
-    description: 'Portfolio site for Chris Pikul, developer for hire',
-    openGraph: {
-        basic: {
-            title: 'Portfolio of Chris Pikul',
-            type: 'website',
-            image: '/chris-logo-512.png',
-        },
-        image: {
-            url: '/chris-logo-1024.png',
-            type: 'image/png',
-            alt: 'Logo for Chris Pikul featuring the monogram C and P',
-        },
-        optional: {
-            description:
-                'Personal portfolio site for Chris Pikul, developer for hire.',
-            locale: 'en-US',
-            siteName: 'Chris Pikul',
-        },
-    },
-    twitter: {
-        card: 'summary',
-        site: '@ChrisPikul510',
-        creator: '@ChrisPikul510',
-    },
-    extend: {
-        link: [{ rel: 'icon', href: '/favicon.svg' }],
-        meta: [
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
+export const MakeSEO = (overrides: SEOProps = {}): SEOProps =>
+    overrideObjects(
+        {
+            title: 'Chris Pikul',
+            description: 'Portfolio site for Chris Pikul, developer for hire',
+            openGraph: {
+                basic: {
+                    title: 'Portfolio of Chris Pikul',
+                    type: 'website',
+                    image: '/chris-logo-512.png',
+                },
+                image: {
+                    url: '/chris-logo-1024.png',
+                    type: 'image/png',
+                    alt: 'Logo for Chris Pikul featuring the monogram C and P',
+                },
+                optional: {
+                    description:
+                        'Personal portfolio site for Chris Pikul, developer for hire.',
+                    locale: 'en-US',
+                    siteName: 'Chris Pikul',
+                },
             },
-        ],
-    },
-
-    ...overrides,
-});
+            twitter: {
+                card: 'summary',
+                site: '@ChrisPikul510',
+                creator: '@ChrisPikul510',
+            },
+            extend: {
+                link: [{ rel: 'icon', href: '/favicon.svg' }],
+                meta: [
+                    {
+                        name: 'viewport',
+                        content: 'width=device-width, initial-scale=1',
+                    },
+                ],
+            },
+        },
+        overrides,
+    );
 export default MakeSEO;
